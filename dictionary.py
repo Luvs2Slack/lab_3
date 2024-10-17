2a90c6ef22577c071aa728c5be6fbb7515b1bb35
Luvs2Slack <145234378+Luvs2Slack@users.noreply.github.com>
2024-10-17 16:57:18 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+the_dict = {"car_make": "volkswagen", "car_model": "shortewned vw beetle"}
+print(the_dict)
+make = the_dict["car_make"]
+print(the_dict)
+the_dict["year"] = 1984
+print(the_dict)
+the_dict["colour"] = "yellow"
+print(the_dict)
+del the_dict["colour"]
